e749c3fcb6dc70f7e1120f95c147555212376920
Parikshit-Coder-create <parikshitrathore1710@gmail.com>
2026-09-16 23:32:53 +0530

    Update Q7.c

---
+++ Q7.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int n, remainder;
+    long long binary = 0, place = 1;
+
+    printf("Enter a positive decimal integer: ");
+    scanf("%d", &n);
+
+    if (n == 0) {
+        printf("Binary = 0\n");
+        return 0;
+    }
+
+    while (n > 0) {
+        remainder = n % 2;
+        binary = binary + remainder * place;
+        place = place * 10;
+        n = n / 2;
+    }
+
+    printf("Binary = %lld\n", binary);
+
+    return 0;
+}
